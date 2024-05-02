41b53f2ee5c4ebc52d2e0afccb5f96741e44feda
sndpjaat <sndpjaat008@gmail.com>
2024-05-02 08:17:56 +0530

    add css

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,24 @@ menuIcon.addEventListener("click" , ()=>{
     menuList.classList.toggle("active");
 
 }) 
+function countdown() {
+    var countDownDate = new Date().getTime() + (19 * 24 * 60 * 60 * 1000) + (10 * 60 * 60 * 1000) + (2 * 60 * 1000) + (55 * 1000);
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        var countdownElement = document.getElementById("countdown");
+        countdownElement.innerHTML = "<span class='color-violet'>" + days + "d</span> <span class='countdown-text'>:</span> " + hours + "hr : " + minutes + "min : " + seconds + "sec";
+
+        if (distance < 0) {
+            clearInterval(x);
+            countdownElement.innerHTML = "Countdown expired!";
+        }
+    }, 1000);
+}
